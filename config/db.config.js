c0618230ed34daff5516f12b6a4f7d7173d8271f
tushar1977 <steve192g@gmail.com>
2024-04-09 17:42:28 +0530

    done changes

--- config/db.config.js
+++ config/db.config.js
@@ -6,9 +6,6 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}`,
       { writeConcern: { w: "majority" } },
     );
-    console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`,
-    );
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
     process.exit(1);
